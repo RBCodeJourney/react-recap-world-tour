a244812762c706bc2c3cadc6b2dcf02650b398bf
RBCodeJourney <refatullahbiswas@gmail.com>
2024-01-27 00:16:48 +0530

    country data added displayed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h2>React World tour Recap</h2>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
