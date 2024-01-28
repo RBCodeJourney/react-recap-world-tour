86df569eebc90dba8f1f94adf084e276a27f8d6d
RBCodeJourney <refatullahbiswas@gmail.com>
2024-01-29 01:17:10 +0530

    Prop Drilling, Pass Props In Multi Level

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './Country.css'
+import CountryDetail from '../CountryDetail/CountryDetail';
 
-const Country = ({ county }) => {
-    const { name, flags, population, area, cca3 } = county;
+const Country = ({ county: country, handleVisitedCountry, handleVisitedFlags }) => {
+    const { name, flags, population, area, cca3 } = country;
 
     const [Visited, setVisited] = useState(false);
 
@@ -10,16 +11,30 @@ const Country = ({ county }) => {
         setVisited(!Visited)
     }
 
+    // const passWithParams = () => handleVisitedCountry(country);
+
+
+
     return (
-        <div className="country">
-            <h3>{name.common}</h3>
+        <div className={`country ${Visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{ color: Visited ? 'Purple' : 'black' }}>{name.common}</h3>
             <img src={flags?.png} alt="" />
             <p>Population: {population}</p>
             <p>area: {area}</p>
             <p><small>Code:{cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+            <br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            <br />
             <button onClick={handleVisited}>{Visited ? 'Visited' : 'Going'}</button>
             {Visited ? 'I have visited this country.' : 'I want to visit'}
-        </div>
+            <hr />
+            <CountryDetail
+                country={country}
+                handleVisitedCountry={handleVisitedCountry}
+                handleVisitedFlags={handleVisitedFlags}
+            ></CountryDetail>
+        </div >
     );
 };
 
